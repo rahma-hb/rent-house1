7d74ad0c4f3b4de0c585cf6d9f216c33ab34c89c
rahma-hb <104564925+rahma-hb@users.noreply.github.com>
2025-05-30 16:30:13 +0100

    Update footer.js

--- app/components/footer.js
+++ app/components/footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     <footer className="bg-dark text-light">
       <div className="flex flex-col gap-4 items-center py-4">
         <p>RentHouse © 2025</p>
-        <a href="mailto:contact@renthouse.com" className="hover:underline">
+        <a href="/contact" className="hover:underline">
           Contactez-nous
         </a>
         <div className="flex gap-4 text-2xl text-primary">
